e3c7895a8a1999e422b6d11303617224e5922c15
YingKuan <112489587+kuan79223@users.noreply.github.com>
2023-08-13 09:55:17 +0800

    Update globals.py
    新增一個 CFG 的全域變數

--- globals.py
+++ globals.py
@@ -5,6 +5,7 @@ VAL_FOLDER = ''
 # train 指令參數
 PERCENT = 90
 TRAIN_WEIGHTS = ''
+CFG = ''
 DATA = ''
 IMG_SIZE = ['224x224', '320x320', '416x416', '640x640']
 DEVICE = ['CPU', 'GPU 1顆', 'GPU 2顆', 'GPU 3顆']
